03818bdfdfe6a7c58ef30092b7a56bf669bfbb54
zhangshijie <zhangshijie@tidebuy.net>
2018-11-16 14:55:49 +0800

    add ideaFragment

--- app/src/main/java/com/llx/bear/ui/fragment/IdeaFragment.java
+++ app/src/main/java/com/llx/bear/ui/fragment/IdeaFragment.java
@@ -1,12 +1,14 @@
 package com.llx.bear.ui.fragment;
 
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.llx.bear.R;
+import com.llx.bear.databinding.FragmentIdeaBinding;
 import com.llx.bear.ui.base.BaseFragment;
 
 /**
@@ -15,7 +17,7 @@ import com.llx.bear.ui.base.BaseFragment;
  */
 public class IdeaFragment extends BaseFragment {
 
-
+    private FragmentIdeaBinding binding;
     public IdeaFragment() {
         // Required empty public constructor
     }
@@ -24,12 +26,17 @@ public class IdeaFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_idea, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_idea,container,false);
+        return binding.getRoot();
     }
 
     @Override
     public void configView() {
 
+    }
+
+    @Override
+    protected void lazyLoadData() {
+        super.lazyLoadData();
     }
 }
